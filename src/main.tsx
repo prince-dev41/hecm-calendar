618f4605b9328ac502011b5531289dfffd5d28b4
prince-dev41 <princeekpinse97+1@gmail.com>
2025-03-10 18:59:58 +0100

    Config Posthog for analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { AuthProvider } from './contexts/AuthContext';
-import App from './App.tsx';
-import './index.css';
-import { Toaster } from 'react-hot-toast';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { PostHogProvider } from 'posthog-js/react'
+import { AuthProvider } from './contexts/AuthContext'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AuthProvider>
-      <App />
-      <Toaster position="top-right" />
-    </AuthProvider>
-  </StrictMode>
-);
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <PostHogProvider 
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </PostHogProvider>
+  </React.StrictMode>,
+)
